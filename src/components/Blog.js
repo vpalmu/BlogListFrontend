5431073fa1ac87b7b3f43d40977bf1fbf5268b43
DESKTOP-0NLE5N9\vesa.palmu <vesa.palmu@alfame.com>
2023-03-25 14:40:22 +0200

    blog details view & hide

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,28 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} by {blog.author}
-  </div>
-);
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+
+  const [visible, setVisible] = useState(false);
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  const showDetails = () => (
+    <div className='details'>
+      <p>Id: { blog.id } </p>
+      <p>Likes: { blog.likes } </p>
+      <p>Url: { blog.url } </p>
+      <p>Author: { blog.author } </p>
+      <button onClick={toggleVisibility}>hide</button>
+    </div>
+  );
+
+  return(
+    <div>
+      { blog.title} by {blog.author} <button onClick={toggleVisibility}>view</button>
+      { visible === true && showDetails() }
+    </div>
+  );
+};
 
 export default Blog;
